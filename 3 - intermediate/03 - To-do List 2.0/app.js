9609a7788dd57d5c1ec9e754605fa7899b8bf61f
Aecio Neto <aecyoneto@gmail.com>
2023-01-27 12:29:16 -0300

    DOM manipulation modifications

--- 3 - intermediate/03 - To-do List 2.0/app.js
+++ 3 - intermediate/03 - To-do List 2.0/app.js
@@ -1,12 +1,14 @@
 const themeToggleBtn = document.querySelector('#theme-toggle-btn')
 const themeToggleDarkIcon = document.querySelector('#theme-toggle-dark-icon')
 const themeToggleLightIcon = document.querySelector('#theme-toggle-light-icon')
+
 const todoInput = document.querySelector('#todoInput')
 const form = document.querySelector('form');
-const todosContainer = document.querySelector('#todosContainer');
-const todoItems = document.querySelectorAll('li');
+const todosContainer = document.querySelector('#todosContainer')
+const listItem = document.querySelectorAll('.listItem')
+const trashIcons = document.querySelectorAll('.trashIcon')
 
-console.log(todoInput, form, todosContainer, todoItems)
+// console.log(todoInput, form, todosContainer, todoItems)
 
 let input = ''
 
@@ -60,52 +62,89 @@ c) Inserir no DOM o html com o texto/for/id - ok
   modificar a estrutura do html +=
   é possível fazer um appendChild também.
 
-d) mostrar o trash button X quando houver um hover na Li
+d) mostrar o trash button X quando houver um hover na Li - OK
 
-e) deletar tarefas quando clicado no X
+e) deletar tarefas quando clicado no X - Ok
 
-f) riscar o texto quando o input é marcado com checked
+f) fazer as lis/tarefas inseridas serem interativas - em andamento
 
-e) fazer as lis/tarefas inseridas serem interativas
+g) riscar o texto quando o input é marcado com checked
 
+y) Bugs encontrados: X na versão mobile
+
+z) refatorar código
 
 */
 
-todoInput.addEventListener('input', event => {
-  input = event.target.value
-  
-})
+const createTodoItem = () => {
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  console.log(input)
-
-  const todoItem = `
-  <li>
-    <div class="bg-white dark:bg-VeryDarkDesaturatedBlue h-16 pl-6 flex items-center border-b-LightGrayisBlue">
-      <div class="p-[.04em] bg-gradient-to-r from-white to-white hover:from-[#8C9DFB] hover:to-[#BA63F1] rounded-full hover:cursor-pointer" id="checkbox-border-effect">
-        <input class="rounded-full border-LightGrayishBlue dark:bg-VeryDarkDesaturatedBlue p-3 hover:cursor-pointer checked:bg-gradient-to-r from-[#8C9DFB] to-[#BA63F1]" type="checkbox" name="" id="${input}">
-      </div>
-      <label class="pt-2 ml-3 hover:cursor-pointer flex-grow dark:text-LightGrayishBlue" for="${input}">${input}</label>
-      <img class="hover:cursor-pointer mx-2 hidden" src="images/icon-cross.svg" alt="">
-    </div>
-  </li>`
-
-  todosContainer.innerHTML += todoItem
-
-  form.reset()
-
-})
-
-// conseguir a referência das classes para remover o hidden do trash icon.
-
-todoItems.forEach(li =>{
-  li.addEventListener('mouseover', e => {
-    console.log(li.childNodes)
+  todoInput.addEventListener('input', event => {
+    input = event.target.value
+    
   })
-})
+
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log(input)
+
+    const newTodoItem = document.createElement('li')
+    
+    const p = document.createElement('p')
+    p.innerText = 'Inserido Before oque?'
+    
+    newTodoItem.innerHTML = `
+      <div class="dark:bg-VeryDarkDesaturatedBlue h-16 pl-6 flex items-center border-b-LightGrayisBlue">
+        <div class="p-[.04em] bg-gradient-to-r from-white to-white hover:from-[#8C9DFB] hover:to-[#BA63F1] rounded-full hover:cursor-pointer" id="checkbox-border-effect">
+          <input class="rounded-full border-LightGrayishBlue dark:bg-VeryDarkDesaturatedBlue p-3 hover:cursor-pointer checked:bg-gradient-to-r from-[#8C9DFB] to-[#BA63F1]" type="checkbox" name="" id="${input}">
+        </div>
+        <label class="pt-2 ml-3 hover:cursor-pointer flex-grow dark:text-LightGrayishBlue" for="${input}">${input}</label>
+        <img class="hover:cursor-pointer mx-2 hidden" src="images/icon-cross.svg" alt="">
+      </div>`
+
+      todosContainer.appendChild(newTodoItem)
+
+    form.reset()
+
+  })
+}
+
+const showTrashIcon = () =>{
+  listItem.forEach(listItem => {
+    const trashIcon = listItem.lastElementChild
+    
+    listItem.addEventListener('mouseover', event => {
+      if (trashIcon.classList.contains('hidden')) {
+        listItem.lastElementChild.classList.remove('hidden')
+      }
+    })
+  })
+}
+
+const hideTrashIcon = () => {
+  listItem.forEach(listItem => {
+    const trashIcon = listItem.lastElementChild
+    listItem.addEventListener('mouseout', event => {
+      if (!trashIcon.classList.contains('hidden')) {
+        listItem.lastElementChild.classList.add('hidden')
+      }
+    })
+  })
+}
+
+const deleteTodoItem = () => {
+  trashIcons.forEach(item => {
+    item.addEventListener('click', event => {
+      item.parentElement.parentNode.remove()
+    })
+  })
+}
+
 
 checkLocalStorageTheme()
+createTodoItem()
+showTrashIcon()
+hideTrashIcon()
+deleteTodoItem()
 
 
 
