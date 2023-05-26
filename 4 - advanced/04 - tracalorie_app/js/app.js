8efe2fc0adfcf57d0f6cc3a46358e2ac192ff1bb
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2023-05-26 14:13:21 -0300

    add Meal Panel

--- 4 - advanced/04 - tracalorie_app/js/app.js
+++ 4 - advanced/04 - tracalorie_app/js/app.js
@@ -8,13 +8,14 @@ class App {
   constructor() {
     this.calorieTracker = new CalorieTracker()
     this.infoPanel = new InfoPanel()
+    this.mealPanel = new MealPanel()
   }
-
+  
   start() {
     console.log('comecei do zero')
+    this.setupEventListeners()
     this.caloriesConsumeTest()
     this.updateUI()
-    this.setupEventListeners()
   }
 
   caloriesConsumeTest() {
@@ -23,7 +24,8 @@ class App {
   }
 
   updateUI() {
-    this.infoPanel.updateUI(this.calorieTracker.dailyLimit, this.calorieTracker.caloriesConsumed, this.calorieTracker.caloriesSpent)
+    const { dailyLimit, caloriesConsumed, caloriesSpent } = this.calorieTracker
+    this.infoPanel.updateUI(dailyLimit, caloriesConsumed, caloriesSpent)
   }
 
   // Configura os listeners e a interação com usuário 
@@ -46,12 +48,29 @@ class App {
       console.log('resetDayButton pressionado')
       this.updateUI()
     })
+
+    // Atualiza as calorias com base nas meals inseridas
+    const addMealform = document.querySelector('#meal-form')
+    addMealform.addEventListener('submit', e => {
+      e.preventDefault()
+
+      const mealName = document.querySelector('#meal-name').value
+      const mealCalories = document.querySelector('#meal-calories').value
+      console.log(mealName, mealCalories)
+
+      if (mealName && mealCalories) {
+        const meal = new Meal(mealName, mealCalories)
+        this.mealPanel.addMeal(meal)
+        this.calorieTracker.addCaloriesConsumed(mealCalories)
+        this.updateUI()
+      }
+    })
   }
 }
 
 class CalorieTracker {
   constructor(dailyLimit, caloriesConsumed, caloriesSpent) {
-    this.dailyLimit = 2000 //match o html, isso muda com o localStorage
+    this.dailyLimit = 2000 
     this.caloriesConsumed = 0
     this.caloriesSpent = 0
   }
@@ -83,6 +102,14 @@ class CalorieTracker {
   addCaloriesSpent(calories) {
     if (!isNaN(calories) && calories > 0) {
       this.caloriesSpent += parseInt(calories)
+    } else {
+      console.log('Informe um valor de calorias válido!')
+    }
+  }
+
+  removeCaloriesConsumed(calories) {
+    if (!isNaN(calories) && calories > 0) {
+      this.caloriesConsumed -= parseInt(calories)
     } else {
       console.log('Informe um valor de calorias válido!')
     }
@@ -131,15 +158,106 @@ class InfoPanel {
   updateProgressBar(dailyLimit, caloriesConsumed, caloriesSpent) {
     const totalCalories = dailyLimit + caloriesSpent
     const remainingCalories = totalCalories - caloriesConsumed
-    const percentage = ((totalCalories - remainingCalories) / totalCalories) * 100;
-    this.progressBar.style.width = `${percentage}%`;
+    const percentage = ((totalCalories - remainingCalories) / totalCalories) * 100
+    this.progressBar.style.width = `${percentage}%`
     this.progressBar.classList.toggle('bg-danger', remainingCalories < 0)
     this.progressBar.classList.toggle('bg-success', remainingCalories >= 0)
     this.caloriesRemainingEl.parentElement.classList.toggle('bg-danger', remainingCalories < 0)
     this.caloriesRemainingEl.parentElement.classList.toggle('bg-success', remainingCalories >= 0)
   }
-
 }
+
+// Preciso inserir as refeições
+// Isso é feito através do clique em um botão. 
+// As refeições possuem Nome e Calorias
+// As refeições são adicionadas ao painel de refeições
+// As calorias afetam a CalorieTracker. (O infoPanel já está ligado à calorieTracker)
+// Após a inserção ou remoção o painel precisa ser atualizado. 
+
+// as refeições esão sendo inseridas
+// calorias contabilizadas (adição)
+// a adição das calorias ocorre no calorieTracker
+// quero que o render fique em MealPanel
+// quero que as calorias sejam contabilizadas na App
+// O que mudar?
+
+class MealPanel {
+  constructor() {
+    this.meals = []
+    this.mealsItems = document.querySelector('#meal-items')
+  }
+
+  addMeal(meal) {
+    this.meals.push(meal)
+    this.renderMeal(meal)
+  }
+
+  removeMeal(meal) {
+    const index = this.meals.indexOf(meal)
+    if (index !== -1) {
+      this.meals.splice(index, 1)
+      meal.removeElement()
+    }
+  }
+
+  renderMeal(meal) {
+    const mealItem = document.createElement('div')
+    mealItem.className = 'card my-2'
+    mealItem.innerHTML = `
+      <div class="card-body">
+        <div class="d-flex align-items-center justify-content-between">
+          <h4 class="mx-1">${meal.name}</h4>
+          <div class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5">
+            ${meal.calories}
+          </div>
+          <button class="delete btn btn-danger btn-sm mx-2">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+      </div>
+    `
+    this.mealsItems.appendChild(mealItem)
+
+    const deleteButton = mealItem.querySelector('.delete')
+    deleteButton.addEventListener('click', () => {
+      this.removeMeal(meal)
+    })
+
+    meal.setElement(mealItem)
+  }
+
+  // updateTotalCalories() {
+  //   let totalCalories = 0
+  //   for (const meal of this.meals) {
+  //     totalCalories += meal.calories
+  //   }
+  //   this.updateTotalCaloriesUI(totalCalories)
+  // }
+
+  // updateTotalCaloriesUI(totalCalories) {
+  //   const totalCaloriesEl = document.querySelector('#total-calories')
+  //   totalCaloriesEl.textContent = totalCalories
+  // }
+}
+
+class Meal {
+  constructor(name, calories) {
+    this.name = name
+    this.calories = calories
+    this.element = null
+    this.calorieTracker = new CalorieTracker()
+  }
+
+  setElement(element) {
+    this.element = element
+  }
+
+  removeElement() {
+    this.element.remove();
+  }
+}
+
+
 
 const app = new App()
 app.start()
