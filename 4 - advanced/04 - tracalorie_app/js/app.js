a1ac022e24c5e07876cc71874395248f6c5d0be5
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2023-05-26 11:17:23 -0300

    add progressBar UI

--- 4 - advanced/04 - tracalorie_app/js/app.js
+++ 4 - advanced/04 - tracalorie_app/js/app.js
@@ -16,12 +16,19 @@ class App {
 
   start() {
     console.log('comecei do zero')
+    this.caloriesConsumeTest()
     this.updateUI()
     this.setupEventListeners()
   }
 
+  caloriesConsumeTest() {
+    this.calorieTracker.addCaloriesConsumed(2100)
+    this.calorieTracker.addCaloriesSpent(0)
+  }
+
   updateUI() {
-    this.infoPanel.updateUI(this.calorieTracker.dailyLimit, this.calorieTracker.caloriesConsumed, this.calorieTracker.caloriesSpent);
+    this.infoPanel.updateUI(this.calorieTracker.dailyLimit, this.calorieTracker.caloriesConsumed, this.calorieTracker.caloriesSpent)
+    this.infoPanel.updateProgressBar(this.calorieTracker.dailyLimit, this.calorieTracker.caloriesConsumed, this.calorieTracker.caloriesSpent)
   }
 
   // Configura os listeners e a interação com usuário 
@@ -49,7 +56,7 @@ class App {
 
 class CalorieTracker {
   constructor(dailyLimit, caloriesConsumed, caloriesSpent) {
-    this.dailyLimit = 0
+    this.dailyLimit = 2000 //match o html, isso muda com o localStorage
     this.caloriesConsumed = 0
     this.caloriesSpent = 0
   }
@@ -71,13 +78,31 @@ class CalorieTracker {
     console.log('resetDay CalorieTracker: disparado')
   }
 
+
+  addCaloriesConsumed(calories) {
+    // Verificar se as calorias informadas são um número válido
+    if (!isNaN(calories) && calories > 0) {
+      this.caloriesConsumed += parseInt(calories)
+    } else {
+      console.log('Informe um valor de calorias válido!')
+    }
+  }
+
+  addCaloriesSpent(calories) {
+    // Verificar se as calorias informadas são um número válido
+    if (!isNaN(calories) && calories > 0) {
+      this.caloriesSpent += parseInt(calories)
+    } else {
+      console.log('Informe um valor de calorias válido!')
+    }
+  }
+
   getTotalCalories() {
-    return this.dailyLimit + this.caloriesSpent - this.caloriesConsumed;
+    return this.dailyLimit + this.caloriesSpent - this.caloriesConsumed
   }
 
   getCaloriesRemaining() {
-    const total = this.dailyLimit + this.caloriesSpent - this.caloriesConsumed;
-    return total > 0 ? total : 0
+    return this.dailyLimit + this.caloriesSpent - this.caloriesConsumed
   }
 }
 
@@ -87,33 +112,40 @@ class InfoPanel {
     this.dailyLimitEl = document.querySelector('#calories-limit')
     this.caloriesConsumedEl = document.querySelector('#calories-consumed')
     this.caloriesSpentEl = document.querySelector('#calories-spent')
-    this.caloriesRemainingEl = document.querySelector('#calories-remaining');
+    this.caloriesRemainingEl = document.querySelector('#calories-remaining')
     this.totalCaloriesEl = document.querySelector('#calories-total')
+    this.progressBar = document.querySelector('.progress-bar')
   }
 
   updateUI(dailyLimit, caloriesConsumed, caloriesSpent) {
     this.caloriesConsumedEl.textContent = caloriesConsumed
     this.caloriesSpentEl.textContent = caloriesSpent
-    this.totalCaloriesEl.textContent = this.caloriesBalance(dailyLimit, caloriesConsumed, caloriesSpent)
+    this.totalCaloriesEl.textContent = this.caloriesBalance(caloriesConsumed, caloriesSpent)
     this.caloriesRemainingEl.textContent = this.getCaloriesRemaining(dailyLimit, caloriesConsumed, caloriesSpent)
   }
 
-  caloriesBalance(dailyLimit, caloriesConsumed, caloriesSpent) {
-    if (caloriesConsumed === 0 && caloriesSpent === 0) {
-      return 0
-    } else {
-      return dailyLimit - caloriesConsumed + caloriesSpent;
-    }
+  caloriesBalance(caloriesConsumed, caloriesSpent) {
+    return caloriesConsumed - caloriesSpent
   }
 
   getCaloriesRemaining(dailyLimit, caloriesConsumed, caloriesSpent) {
-    const total = dailyLimit + caloriesSpent - caloriesConsumed;
-    return total > 0 ? total : 0
+    return dailyLimit + caloriesSpent - caloriesConsumed
   }
 
   updateDailyLimit(newLimit) {
     this.dailyLimitEl.textContent = newLimit
     console.log('updateDailyLimit do InfoPanel: ativo')
+  }
+
+  updateProgressBar(dailyLimit, caloriesConsumed, caloriesSpent) {
+    const totalCalories = dailyLimit + caloriesSpent
+    const remainingCalories = totalCalories - caloriesConsumed
+    const percentage = ((totalCalories - remainingCalories) / totalCalories) * 100;
+    this.progressBar.style.width = `${percentage}%`;
+    this.progressBar.classList.toggle('bg-danger', remainingCalories < 0)
+    this.progressBar.classList.toggle('bg-success', remainingCalories >= 0)
+    this.caloriesRemainingEl.parentElement.classList.toggle('bg-danger', remainingCalories < 0)
+    this.caloriesRemainingEl.parentElement.classList.toggle('bg-success', remainingCalories >= 0)
   }
 
 }
